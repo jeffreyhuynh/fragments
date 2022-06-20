30967289508b772ab7437f5bd95fae96bd2563c7
JH <>
2022-06-20 17:01:40 -0400

    updated GET /fragments, added expand query

--- src/routes/api/get/index.js
+++ src/routes/api/get/index.js
@@ -1,8 +1,14 @@
 // src/routes/api/get/index.js
 
 const { createSuccessResponse } = require('../../../response');
+const logger = require('../../../logger');
+const { Fragment } = require('../../../model/fragment');
 
-module.exports = (req, res) => {
-  // to be updated to return list of fragments user has
-  res.status(200).json(createSuccessResponse({ fragments: [] }));
+module.exports = async (req, res) => {
+  const userFrags = await Fragment.byUser(req.user, req.query.expand === '1' ? true : false);
+  logger.info(
+    { user: req.user, expand: req.query.expand, fragments: userFrags },
+    'GET /v1/fragments received'
+  );
+  res.status(200).json(createSuccessResponse({ fragments: userFrags }));
 };
